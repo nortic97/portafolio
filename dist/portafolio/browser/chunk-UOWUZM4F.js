cebf1e89933687710f7f79659e0cfdea6277f3d2
dragon <nortic97@gmail.com>
2025-02-15 15:45:01 -0500

    dist generated

---
+++ dist/portafolio/browser/chunk-UOWUZM4F.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-QONH2IUI.js";import"./chunk-ODKXHRBJ.js";var e=[{path:"",component:o}];export{e as routes};
---
+++ dist/portafolio/browser/chunk-W5QZL2NU.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-MPSNILIZ.js";import"./chunk-ODKXHRBJ.js";var m=[{path:"",component:o}];export{m as routes};
